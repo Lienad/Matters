5d406434bbeaebcdea0a66d360484d00b1e5eb58
Daniel Abraham <daniel.abraham@texture.com>
2016-01-08 23:07:35 -0800

    Moved MattersAdapter from the apis package. It did not make sense for it to be there.

--- app/src/main/java/com/daniel/matters/MattersAdapter.java
+++ app/src/main/java/com/daniel/matters/MattersAdapter.java
@@ -1,4 +1,4 @@
-package com.daniel.matters.apis;
+package com.daniel.matters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -6,9 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
-import com.daniel.matters.Matter;
-import com.daniel.matters.R;
 
 import java.util.List;
 
