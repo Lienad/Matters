1098391e348f371c190062c36a43c7aed0ebb868
Daniel Abraham <daniel.abraham@texture.com>
2016-01-08 23:26:45 -0800

    Matter details will now open when a matter is selected from the list of matters.

---
+++ app/src/main/java/com/daniel/matters/activities/MatterDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.daniel.matters.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.daniel.matters.R;
+
+/**
+ * Created by dabraham on 1/8/16.
+ */
+public class MatterDetailsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matter_details);
+    }
+}
