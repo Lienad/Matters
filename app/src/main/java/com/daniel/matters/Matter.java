e1661c7731cf3b447c8c4240a7b895101f602683
Daniel Abraham <daniel.abraham@texture.com>
2016-01-09 00:11:29 -0800

    The matter will be passed on to the activity MatterDetails, and the textviews will be set.

--- app/src/main/java/com/daniel/matters/Matter.java
+++ app/src/main/java/com/daniel/matters/Matter.java
@@ -1,9 +1,12 @@
 package com.daniel.matters;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by dabraham on 1/8/16.
  */
-public class Matter {
+public class Matter implements Parcelable{
 
     long id;
     Client client;
@@ -26,19 +29,44 @@ public class Matter {
     String billing_method;
     long group_id;
     Permission permission;
+
     //ActivityRate activity_rates;
 
-    public class Permission {
-        long id;
-        String url;
-        String name;
+
+    protected Matter(Parcel in) {
+        id = in.readLong();
+        client = in.readParcelable(Client.class.getClassLoader());
+        display_number = in.readString();
+        description = in.readString();
+        status = in.readString();
+        open_date = in.readString();
+        close_date = in.readString();
+        pending_date = in.readString();
+        location = in.readString();
+        client_reference = in.readString();
+        reponsible_attorney = in.readString();
+        originating_attorney = in.readString();
+        practice_area = in.readString();
+        billable = in.readByte() != 0;
+        maildrop_address = in.readString();
+        created_at = in.readString();
+        updated_at = in.readString();
+        billing_method = in.readString();
+        group_id = in.readLong();
+        permission = in.readParcelable(Permission.class.getClassLoader());
     }
 
-    public class Client {
-        long id;
-        String url;
-        String name;
-    }
+    public static final Creator<Matter> CREATOR = new Creator<Matter>() {
+        @Override
+        public Matter createFromParcel(Parcel in) {
+            return new Matter(in);
+        }
+
+        @Override
+        public Matter[] newArray(int size) {
+            return new Matter[size];
+        }
+    };
 
     public Long getId() {
         return id;
@@ -50,6 +78,115 @@ public class Matter {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getOpenDate() {
+        return open_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeParcelable(client, 0);
+        dest.writeString(display_number);
+        dest.writeString(description);
+        dest.writeString(status);
+        dest.writeString(open_date);
+        dest.writeString(close_date);
+        dest.writeString(pending_date);
+        dest.writeString(location);
+        dest.writeString(client_reference);
+        dest.writeString(reponsible_attorney);
+        dest.writeString(originating_attorney);
+        dest.writeString(practice_area);
+        dest.writeInt(billable ? 1 : 0);
+        dest.writeString(maildrop_address);
+        dest.writeString(created_at);
+        dest.writeString(updated_at);
+        dest.writeString(billing_method);
+        dest.writeLong(group_id);
+        dest.writeParcelable(permission, 0);
+    }
+
+    public static class Permission implements Parcelable {
+        long id;
+        String url;
+        String name;
+
+        protected Permission(Parcel in) {
+            id = in.readLong();
+            url = in.readString();
+            name = in.readString();
+        }
+
+        public static final Creator<Permission> CREATOR = new Creator<Permission>() {
+            @Override
+            public Permission createFromParcel(Parcel in) {
+                return new Permission(in);
+            }
+
+            @Override
+            public Permission[] newArray(int size) {
+                return new Permission[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeLong(id);
+            dest.writeString(url);
+            dest.writeString(name);
+        }
+    }
+
+    public static class Client implements Parcelable {
+        long id;
+        String url;
+        String name;
+
+        protected Client(Parcel in) {
+            id = in.readLong();
+            url = in.readString();
+            name = in.readString();
+        }
+
+        public static final Creator<Client> CREATOR = new Creator<Client>() {
+            @Override
+            public Client createFromParcel(Parcel in) {
+                return new Client(in);
+            }
+
+            @Override
+            public Client[] newArray(int size) {
+                return new Client[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeLong(id);
+            dest.writeString(url);
+            dest.writeString(name);
+        }
     }
 
 
@@ -74,5 +211,6 @@ public class Matter {
 //            String activity_description;
 //        }
 //    }
+
 
 }
--- app/src/main/java/com/daniel/matters/activities/MatterDetailsActivity.java
+++ app/src/main/java/com/daniel/matters/activities/MatterDetailsActivity.java
@@ -1,18 +1,79 @@
 package com.daniel.matters.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.daniel.matters.Matter;
 import com.daniel.matters.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by dabraham on 1/8/16.
  */
-public class MatterDetailsActivity extends Activity {
+public class MatterDetailsActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.matter_id)
+    TextView matterId;
+
+    @Bind(R.id.client_name)
+    TextView clientName;
+
+    @Bind(R.id.matter_description)
+    TextView matterName;
+
+    @Bind(R.id.matter_open_date)
+    TextView matterOpenDate;
+
+    @Bind(R.id.matter_status)
+    TextView matterStatus;
+
+    Matter matter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matter_details);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        matter = getIntent().getParcelableExtra("matter");
+        Log.e("TAG", "matter id: " + matter.getId());
+        setFields();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_matters, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setFields() {
+        matterId.setText(Long.toString(matter.getId()));
+        clientName.setText(matter.getClientName());
+        matterName.setText(matter.getDescription());
+        matterOpenDate.setText(matter.getOpenDate());
+        matterStatus.setText(matter.getStatus());
     }
 }
